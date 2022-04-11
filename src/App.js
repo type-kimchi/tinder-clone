0e8929d0b8c20f9faf06cd9b98db5c0f71f654cb
type-kimchi <swgood4@gmail.com>
2022-04-11 20:54:21 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Header from './header';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Let's build the Tinder Clone app 🚀🚀🚀</h1>
+
+     {/* header */}
+     <Header />;
+     {/* tinder cards */}
+     {/* button belows tinder cards */}
+
+     {/* Chat screen */}
+     {/* individual chat screen */}
+
+
+    </div>
+  );
+}
+
+export default App;
